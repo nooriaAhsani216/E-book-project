656c0fa987675c40ca6d08c647099b2e1458ae06
nooriaAhsani216 <nooriaahsani216@gmail.com>
2025-04-07 11:47:53 +0430

    SignIn component added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
+import SignIn from './Sign-in'
 
 export default function Header() {
   return (
-    <div className = "bg-amber-600">
+    <div className = "bg-amber-600 md:flex justify-between">
+       <p>BookByte</p>
 <Navbar />
+<SignIn />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,33 +2,37 @@ import React from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { IoMdClose } from 'react-icons/io';
 import { useState } from 'react';
-import {headerData} from '@/lib/headerData';
+import { headerData } from '@/lib/headerData';
+import SignIn from './Sign-in';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   function toggleMenu() {
     setIsOpen(!isOpen);
-  } 
+  }
   return (
-<div>
-        <div className = "bg-red-800 md:hidden"> 
-           {isOpen ? (
-            <IoMdClose  size={30} onClick = {toggleMenu}/>
-          
-          ):(
-            <GiHamburgerMenu size={30} onClick = {toggleMenu}/>
-          )} 
-          {isOpen && (
-            <div>
-{headerData.map((d,i)=>{
-          return(
-            <li>{d.title}</li>
-          )
-        })}
-            </div>
-          )}  
-        </div>
-        
+    <div className="flex flex-col justify-between bg-green-400">
+     
+      <div className="md:hidden">
+        {isOpen ? (
+          <IoMdClose size={30} onClick={toggleMenu} />
+        ) : (
+          <GiHamburgerMenu size={30} onClick={toggleMenu} />
+        )}
+      </div>
+       <div>
+        {isOpen && (
+          <ul>
+            {headerData.map((d, i) => {
+              return (
+                <li>{d.title}</li>
+              )
+            })}
+          </ul>
+        )}
+      </div>
+     
+
     </div>
   )
 }
